5e771ed27bd9fc577d42f273c25d12df3659430a
shivsingh98 <singhshiv0402@gmail.com>
2024-05-06 18:19:06 +0530

    adding landing page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css"
 import LandingPage from "./pages/LandingPage/LandingPage";
 
+
 function App() {
   return (
     <>
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,10 +1,35 @@
 import Navbar from "../../components/Navbar";
 import "./lp.css";
+import Container from "react-bootstrap/Container";
 
 const LandingPage = () => {
+  const data = ["All Products", "Electronics", "Sports", "Kitchenware"];
   return (
     <>
       <Navbar />
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h2 className="home-title">Welcome to online Shopping...</h2>
+          </div>
+          <div className="col-12">
+            <div className="category-list">
+             
+              {
+                data.map((category, index)=>{
+                  return(
+                    <>
+                     <div className="category-item" key={index}>
+                <a href="/products">{category}</a>
+              </div>
+                    </>
+                  )
+                })
+              }
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
